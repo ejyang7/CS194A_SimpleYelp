4e21dd3a4eef02fe9b8a4c4fa8c1ccea5594e38f
ejyang7 <ejyang7@gmail.com>
2020-11-07 21:51:41 -0800

    Using Retrofit to make a network call (up to 23:00 in video)

--- app/src/main/java/edu/stanford/ejyang/simpleyelp/MainActivity.kt
+++ app/src/main/java/edu/stanford/ejyang/simpleyelp/MainActivity.kt
@@ -2,12 +2,38 @@ package edu.stanford.ejyang.simpleyelp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+private const val TAG = "MainActivity"
+private const val BASE_URL = "https://api.yelp.com/v3/"
+private const val API_KEY = "SD-K2vVXogXeJrdUAWozo-67JXIfwSX4VxccQSIAXNC6_PzsL1S6pdoa-m88VpCLqI0F5vUzLKa3V1nUnmekUY9xXvhtnhhZOjSKwHAhvkMztWA2jjINJY5xPHynX3Yx"
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val retrofit =
+                Retrofit.Builder().baseUrl(BASE_URL).addConverterFactory(GsonConverterFactory.create())
+                        .build()
+        val yelpService = retrofit.create(YelpService::class.java)
+        yelpService.searchRestaurants("Bearer $API_KEY","Avocado Toast", "New York").enqueue(object : Callback<Any> {
+            override fun onResponse(call: Call<Any>, response: Response<Any>) {
+                Log.i(TAG, "onResponse $response")
+            }
+
+            override fun onFailure(call: Call<Any>, t: Throwable) {
+                Log.i(TAG, "onFailure $t")
+            }
+
+        })
+        //
+
     }
 }
 
-//hi
