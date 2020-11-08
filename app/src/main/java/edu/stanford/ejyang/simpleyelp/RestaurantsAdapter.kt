a6db6c3d993013879a00f418740434f0311d6479
ejyang7 <ejyang7@gmail.com>
2020-11-08 10:22:25 -0800

    Populating RecyclerView correctly and updated color scheme

--- app/src/main/java/edu/stanford/ejyang/simpleyelp/RestaurantsAdapter.kt
+++ app/src/main/java/edu/stanford/ejyang/simpleyelp/RestaurantsAdapter.kt
@@ -6,6 +6,10 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.view.menu.ActionMenuItemView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.CenterCrop
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+import com.bumptech.glide.request.RequestOptions
 import kotlinx.android.synthetic.main.item_restaurant.view.*
 
 class RestaurantsAdapter(val context: Context, val restaurants: List<YelpRestaurant>) :
@@ -25,6 +29,15 @@ class RestaurantsAdapter(val context: Context, val restaurants: List<YelpRestaur
     inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         fun bind(restaurant: YelpRestaurant) {
             itemView.tvName.text = restaurant.name
+            itemView.ratingBar.rating = restaurant.rating.toFloat()
+            itemView.tvNumReviews.text = "${restaurant.numReviews} Reviews"
+            itemView.tvAddress.text = restaurant.location.address
+            itemView.tvCategory.text = restaurant.categories[0].title
+            itemView.tvDistance.text = restaurant.displayDistance()
+            itemView.tvPrice.text = restaurant.price
+            Glide.with(context).load(restaurant.imageUrl).apply(RequestOptions().transform(
+                CenterCrop(), RoundedCorners(20)
+            )).into(itemView.imageView)
         }
 
     }
